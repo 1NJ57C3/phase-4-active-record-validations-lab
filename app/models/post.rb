e60ccb607f055b570eb98a8308df4c161ecf6172
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-02-02 10:03:55 -0800

    commit tree f46057f5561ff4c276a6c62d016c3f411e112a53

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+    validate :is_clickbait_enough
+
+    def is_clickbait_enough
+        if title.present? && title.exclude?("Won't Believe" || "Secret" || "Top " || "Guess")
+            errors.add(:title, "Needs moar clickbait!")
+        end
+    end
 end
